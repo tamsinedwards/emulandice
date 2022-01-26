f62673052f92dc2042637513946b78483ef5785e
Bob Kopp <robert.kopp@rutgers.edu>
2022-01-25 20:29:11 -0500

    tweaks

--- makeNewInstance.sh
+++ makeNewInstance.sh
@@ -17,17 +17,17 @@ cp -r $MAINDIR/NAMESPACE $MAINDIR/R $MAINDIR/README.md $MAINDIR/inst $MAINDIR/ma
 echo Package: $INSTANCENAME > $INSTANCEDIR/DESCRIPTION
 cat $MAINDIR/template/DESCRIPTION.foot >> $INSTANCEDIR/DESCRIPTION
 
-echo '#!/bin/bash
+echo "#!/bin/bash
 
-    cd ..
-    R CMD INSTALL --no-multiarch --with-keep.source ' $INSTANCENAME '
-    cd ' $INSTANCENAME '
-    Rscript steer/steer.R
-    R CMD REMOVE ' $INSTANCENAME > $INSTANCEDIR/run.sh
+cd ..
+R CMD INSTALL --no-multiarch --with-keep.source $INSTANCENAME
+cd $INSTANCENAME
+Rscript steer/steer.R
+R CMD REMOVE $INSTANCENAME" > $INSTANCEDIR/run.sh
 
-echo 'library('$INSTANCENAME')
-main(packagename="'$INSTANCENAME'", dataset="FACTS",N_temp=501L, outdir="'$RESULTSDIR'") # do only 501 samples for year 2100
-#main(packagename="'$INSTANCENAME'", "timeseries", dataset="FACTS", outdir="'$RESULTSDIR'")' > $INSTANCEDIR/steer/steer.R
+echo "library('$INSTANCENAME')
+main(packagename=\"$INSTANCENAME\", dataset=\"FACTS\",N_temp=501L, outdir=\"$RESULTSDIR\") # do only 501 samples for year 2100
+#main(packagename=\"$INSTANCENAME\", \"timeseries\", dataset=\"FACTS\", outdir=\"$RESULTSDIR\")" > $INSTANCEDIR/steer/steer.R
 
 cat $MAINDIR/template/FACTS_CLIMATE_FORCING.csv.head > $INSTANCEDIR/inst/extdata/FACTS_CLIMATE_FORCING.csv
 cat $MAINDIR/template/FACTS_CLIMATE_FORCING.csv.example >> $INSTANCEDIR/inst/extdata/FACTS_CLIMATE_FORCING.csv
